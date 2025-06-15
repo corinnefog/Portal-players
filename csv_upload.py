d01198ba7eba3281c5b1a3f160f1492ca6f5d453
corinnefog <qfr4cu@virginia.edu>
2025-06-15 18:39:27 -0400

    csv changes

--- csv_upload.py
+++ csv_upload.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 import os
 
-csv_path = os.path.expanduser("~/Downloads/Tomsox Data/TomSox 05-31-2025_cleaned.csv")
+csv_path = os.path.expanduser("~/Downloads/Tomsox Data/TomSox 6-12-2025_merged.csv")
 
 # Load as CSV, not Excel
 df = pd.read_csv(csv_path, encoding='latin1', engine='python', on_bad_lines='skip')
