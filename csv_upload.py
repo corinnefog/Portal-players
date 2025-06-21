5eedf009fe2dd08d6cc95166a0e1b0bd86d286e7
corinnefog <qfr4cu@virginia.edu>
2025-06-21 09:47:32 -0400

    June 20 adds

--- csv_upload.py
+++ csv_upload.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 import os
 
-csv_path = os.path.expanduser("~/Downloads/Tomsox Data/TomSox 6-19-2025_Booton_Eldridge.csv")
+csv_path = os.path.expanduser("~/Downloads/Tomsox Data/TomSox 6-20-2025_DeRossi.csv")
 
 # Load as CSV, not Excel
 df = pd.read_csv(csv_path, encoding='latin1', engine='python', on_bad_lines='skip')
